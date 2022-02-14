63ac81e487bf0a05fc0750e3e6c5c70b4ba05143
Jacob Duijzer <github@duijzer.com>
2022-02-14 08:52:32 +0100

    Ignoring auto custom.el

--- init.el
+++ init.el
@@ -10,15 +10,3 @@
     (set-frame-size (selected-frame) (truncate a-width)  (truncate a-height) t)))
 (setq frame-resize-pixelwise t)
 (my/set-initial-frame)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
