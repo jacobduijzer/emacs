cef22b27d34d0d0b9696e06c69cefcaeb327fc57
Jacob Duijzer <jacob@duijzer.com>
2022-11-14 20:59:30 +0100

    feat: new emacs configuration

--- init.el
+++ init.el
@@ -1,1 +1,18 @@
+;(org-babel-load-file
+; (expand-file-name
+;  "readme.org"
+;  user-emacs-directory))
 (org-babel-load-file "~/.emacs.d/readme.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(toc-org org-bullets vterm writeroom-mode polymode poly evil-collection which-key vertico use-package telephone-line smart-mode-line rust-mode restclient plantuml-mode ox-reveal ox-hugo ox-gfm org-roam org-modern org-make-toc ob-mermaid no-littering mood-one-theme mood-line modus-themes marginalia latex-extra impatient-mode general format-all evil emojify doom-modeline diminish dashboard counsel-projectile all-the-icons)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
