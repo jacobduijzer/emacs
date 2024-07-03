84c8968a6ccb4fe6a428299061e02fa1f265d90a
Jacob Duijzer <jacob@duijzer.com>
2024-07-03 13:26:58 +0200

    Cleanup config, splitting out macros

--- init.el
+++ init.el
@@ -1,32 +1,6 @@
-;(org-babel-load-file
-; (expand-file-name
-;  "readme.org"
-;  user-emacs-directory))
-(org-babel-load-file "~/.emacs.d/readme.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(nerd-icons-completion consule consult-org-roam consult-ripgrep deadgrep ef-themes eat nerd-icons-ibuffer nerd-icons-dired toc-org org-bullets vterm writeroom-mode polymode poly evil-collection which-key vertico use-package telephone-line smart-mode-line rust-mode restclient plantuml-mode ox-reveal ox-hugo ox-gfm org-roam org-modern org-make-toc ob-mermaid no-littering mood-one-theme mood-line modus-themes marginalia latex-extra impatient-mode general format-all evil emojify doom-modeline diminish dashboard counsel-projectile all-the-icons)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
-;; Install straight.el
-;;(defvar bootstrap-version)
-;;(let ((bootstrap-file
-;;       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-;;      (bootstrap-version 6))
-;;  (unless (file-exists-p bootstrap-file)
-;;    (with-current-buffer
-;;        (url-retrieve-synchronously
-;;         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-;;         'silent 'inhibit-cookies)
-;;      (goto-char (point-max))
-;;      (eval-print-last-sexp)))
-;;  (load bootstrap-file nil 'nomessage))
+;; custom macros
+(org-babel-load-file "~/.emacs.d/macros.org")
+
+;; load emacs config
+(org-babel-load-file "~/.emacs.d/readme.org")
