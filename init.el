1620ba31ea44cbad080f4f229ce31c72bbb7fb96
Jacob Duijzer <jacob@duijzer.com>
2024-09-10 16:12:24 +0200

    Trying Vertico, Consult, etc.

--- init.el
+++ init.el
@@ -26,7 +26,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(all-the-icons-dired octicons yasnippet which-key toc-org telephone-line projectile plantuml-mode ox-hugo ox-gfm org-wild-notifier org-superstar org-roam org-modern org-bullets org-appear olivetti nerd-icons-ibuffer nerd-icons-dired nerd-icons-completion modus-themes mixed-pitch magit ivy-rich general evil-nerd-commenter evil-collection diminish dashboard counsel company-box all-the-icons)))
+   '(wgrep embark-consult embark consult vertico-posframe vertico-quick orderless marginalia vertico all-the-icons-dired octicons yasnippet which-key toc-org telephone-line projectile plantuml-mode ox-hugo ox-gfm org-wild-notifier org-superstar org-roam org-modern org-bullets org-appear olivetti nerd-icons-ibuffer nerd-icons-dired nerd-icons-completion modus-themes mixed-pitch magit ivy-rich general evil-nerd-commenter evil-collection diminish dashboard counsel company-box all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
